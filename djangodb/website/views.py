35c61730a9283f01840c63d32422188620662585
DJ <devendrapathak024@gmail.com>
2024-08-17 10:02:02 +0530

    making secondary branch to test

--- djangodb/website/views.py
+++ djangodb/website/views.py
@@ -37,48 +37,62 @@ def join(request):
 # ********************************************************************************************************************************************
 
 
+import os
+from tempfile import NamedTemporaryFile
+
 def audit(request):
     processed_data = None
-    x = 'msg not received'
+    message = 'msg not received'
+    form = CSVUploadForm(request.POST or None, request.FILES or None)
+    
     if request.method == 'POST':
-        form = CSVUploadForm(request.POST, request.FILES)
         if form.is_valid():
             input_file = request.FILES['file']
-            if not input_file.name.endswith('xlsx'):
+            if not input_file.name.endswith('.xlsx'):
                 form.add_error('file', ValidationError('File must be in .xlsx format'))
-        else:
-            # processed_data =audit_main(input_file)
-            x = 'file received'
-            processed_data = input_file
-            for data in processed_data:
-                    Audit.objects.create(
-                        asin=data['asin'],
-                        brand_name=data['brand_name'],
-                        generic_name=data['generic_name'],
-                        status=data['status'],
-                        availablity=data['availablity'],
-                        browser_node=data['browser_node'],
-                        title=data['title'],
-                        reviews=data['reviews'],
-                        rating=data['rating'],
-                        deal=data['deal'],
-                        sold_by=data['sold_by'],
-                        buybox=data['buybox'],
-                        image_len=data['image_len'],
-                        main_img_url=data['main_img_url'],
-                        bullet_point_len=data['bullet_point_len'],
-                        bsr1=data['bsr1'],
-                        bsr2=data['bsr2'],
-                        price=data['price'],
-                        MRP=data['MRP'],
-                        description=data['description'],
-                        A_plus=data['A_plus'],
-                        store_link=data['store_link'],
-                    )
+            else:
+                # Save the uploaded file to a temporary location
+                with NamedTemporaryFile(delete=False, suffix='.xlsx') as temp_file:
+                    for chunk in input_file.chunks():
+                        temp_file.write(chunk)
+                    
+                    temp_file_path = temp_file.name
+
+                try:
+                    # Assuming `audit_main` requires a file path
+                    processed_data = audit_main(temp_file_path)
+                    message = 'file received'
+
+                    for data in processed_data:
+                        Audit.objects.create(
+                            asin=data['asin'],
+                            brand_name=data['brand_name'],
+                            generic_name=data['generic_name'],
+                            status=data['status'],
+                            availablity=data['availablity'],
+                            browser_node=data['browser_node'],
+                            title=data['title'],
+                            reviews=data['reviews'],
+                            rating=data['rating'],
+                            deal=data['deal'],
+                            sold_by=data['sold_by'],
+                            buybox=data['buybox'],
+                            image_len=data['image_len'],
+                            main_img_url=data['main_img_url'],
+                            bullet_point_len=data['bullet_point_len'],
+                            bsr1=data['bsr1'],
+                            bsr2=data['bsr2'],
+                            price=data['price'],
+                            MRP=data['MRP'],
+                            description=data['description'],
+                            A_plus=data['A_plus'],
+                            store_link=data['store_link'],
+                        )
+                finally:
+                    # Clean up the temporary file
+                    os.remove(temp_file_path)
+
     else:
         form = CSVUploadForm()
 
-    return render(request, 'audit.html', {'form': form, 'data': processed_data, x : 'x'})
-
-
-
+    return render(request, 'audit.html', {'form': form, 'data': processed_data, 'message': message})
